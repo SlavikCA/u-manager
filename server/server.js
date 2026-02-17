69eb24b58892533e85a355bd108bffa65af25be4
Slavik Fursov <slavikca@gmail.com>
2026-02-16 23:28:51 -0500

    SERVER: update screenshot handling to include timestamp for cache busting and remove old refresh script

--- server/server.js
+++ server/server.js
@@ -81,6 +81,7 @@ app.use(session({
 app.use((req, res, next) => {
   res.locals.user = req.session.user || null;
   res.locals.serverUrl = SERVER_URL;
+  res.locals.now = Date.now();
   next();
 });
 
